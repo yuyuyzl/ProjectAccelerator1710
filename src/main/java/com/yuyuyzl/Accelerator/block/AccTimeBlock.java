723e676fd643e6589f55653529ad4835dc6e5e4f
yuzhongliang22 <yuzhongliang221@gmail.com>
2017-04-13 15:29:04 +0800

    Modified Texture

--- src/main/java/com/yuyuyzl/Accelerator/block/AccTimeBlock.java
+++ src/main/java/com/yuyuyzl/Accelerator/block/AccTimeBlock.java
@@ -9,19 +9,16 @@ import static com.yuyuyzl.Accelerator.AcceleratorMod.MODID;
  * Created by yuyuyzl on 2017/4/10.
  */
 public class AccTimeBlock extends AccMachineBlock{
-    public IIcon HullTop,HullSide;
+    public IIcon HullTop;
     @Override
     public void registerBlockIcons(IIconRegister icon) {
         super.registerBlockIcons(icon);
 
-        HullTop = icon.registerIcon(MODID + ":HullTopAdv");
-        HullSide = icon.registerIcon(MODID + ":HullSideAdv");
+        HullTop = icon.registerIcon(MODID + ":BlockTime");
 
     }
 
     public IIcon getIcon(int facing, int meta) {
-        if(facing==1)return HullTop;
-        if(facing==0)return square;
-        return HullSide;
+        return HullTop;
     }
 }
